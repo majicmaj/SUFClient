e68e9223fe1989b7aff5b62cc32d376ca423e874
Majd Tarbin <majd.tarbin@outlook.com>
2019-08-19 15:45:46 -0400

    fleshed out nav bar

--- sufclient-app/src/App.js
+++ sufclient-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-// import logo from './logo.svg';
 import Search from './Search'
+import Nav from './Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Search>Search</Search>
+      <Nav />
+      <Search />
     </div>
   );
 }
