af2e458ce08e15a9616de54f1b2ab04dcf49aa53
Majd Tarbin <majd.tarbin@outlook.com>
2019-08-19 15:31:18 -0400

    cleaned up App.js

--- sufclient-app/src/App.js
+++ sufclient-app/src/App.js
@@ -6,17 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
-        <span></span>
         <Search>Search</Search>
-        <span></span>
-        {/* <Map>Map</Map>
-        <span></span>
-        <Listing>Page</Listing> */}
-      </div>
-      {/* <Router></Router>
-      <Router></Router>
-      <Router></Router> */}
     </div>
   );
 }
