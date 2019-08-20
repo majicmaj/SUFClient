8aedffbd9ab8ad7699f994465a36588002c227c6
Majd Tarbin <majd.tarbin@outlook.com>
2019-08-20 13:53:00 -0400

    sharing progress and calibrating

--- sufclient-app/src/Home.js
+++ sufclient-app/src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import tesla from './imgs/charging.jpeg'
 import hourly from './imgs/hourly.jpeg'
+import nightly from './imgs/overnight.jpeg'
 import extended from './imgs/extended.jpeg'
 import './Home.css'
 
@@ -10,25 +11,37 @@ class Home extends Component {
 
         return (
             <div>
-                <h2>What can we help you find? We always have alot.</h2>
-                <div className='options'>
-                    <div className='option'>
-                        <img src={hourly} />
-                        <h3>Hourly</h3>
-                        <div />
-                    </div>
-                    <div className='option'>
-                        <img src={extended} />
-                        <h3>Extended</h3>
-                        <div />
-                    </div>
-                    <div className='option'>
-                        <img src={tesla}/>
-                        <h3>Charging Station</h3>
-                        <div />
+                <div className='wrapper'>
+                    <h2>What can we help you find? We always have alot.</h2>
+                    <div className='options'>
+                        <div className='option'>
+                            <img src={hourly} />
+                            <h3>Hourly</h3>
+                            <div />
+                        </div>
+                        <div className='option'>
+                            <img src={nightly} />
+                            <h3>Overnight</h3>
+                            <div />
+                        </div>
+                        <div className='option'>
+                            <img src={extended} />
+                            <h3>Extended</h3>
+                            <div />
+                        </div>
+                        <div className='option'>
+                            <img src={tesla} />
+                            <h3>Charging Station</h3>
+                            <div />
+                        </div>
+                        <div className='option'>
+                            <img src={hourly} />
+                            <h3>Advanced Search</h3>
+                            <div />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div >
 
         )
     }
--- sufclient-app/src/Map.js
+++ sufclient-app/src/Map.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 
 class Map extends Component {
+    initMap = (div) => {
+        map = new google.map.maps.Map(div), {
+            center: {lat: -34, lng: 150},
+            zoom: 8
+        }
+    }
 
     render() {
 
         return (
-        <div>
-            
-        </div>
-        
+            <div>
 
+            </div>
         )
     }
 
